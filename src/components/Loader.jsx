c0ffd3dffa7729348812dd7ddf859c43d1e51250
shivamg829 <shivamgangwar2197@gmail.com>
2025-12-14 20:19:23 +0530

    Feature: Implemented animated weather-themed Loader component

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,8 +1,14 @@
 const Loader = () => {
   return (
     <div className="loader-container">
-      <div className="loader"></div>
-      <p>Fetching weather...</p>
+      <div className="weather-loader">
+        {/* Visual elements for the loader (e.g., sun rays) */}
+        <div className="sun"></div> 
+        <div className="ray ray-1"></div>
+        <div className="ray ray-2"></div>
+        <div className="ray ray-3"></div>
+      </div>
+      <p className="loading-text">Fetching weather data...</p>
     </div>
   );
 };
